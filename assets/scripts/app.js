bf46959e97ef07f898e45533977286f54ba6b675
Umut Türkkan <uoturkkan@gmail.com>
2023-04-04 11:27:07 +0300

    added auto-tab

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,8 +1,8 @@
 const colorForm = document.querySelector("#user-color");
 const inputHex = document.querySelector("#hex");
-const inputR = document.querySelector("#r");
-const inputB = document.querySelector("#g");
-const inputG = document.querySelector("#b");
+const redInput = document.querySelector("#r");
+const greenInput = document.querySelector("#g");
+const blueInput = document.querySelector("#b");
 const resetBtn = document.querySelector("#reset-btn");
 const randomColorBtn = document.querySelector("#random-color");
 
@@ -13,18 +13,18 @@ colorForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
   const outputHex = inputHex.value.toString();
-  const r = inputR.value.toString();
-  const g = inputG.value.toString();
-  const b = inputB.value.toString();
-  const r1 = inputR.value;
-  const g1 = inputG.value;
-  const b1 = inputB.value;
+  const r = redInput.value.toString();
+  const g = greenInput.value.toString();
+  const b = blueInput.value.toString();
+  const r1 = redInput.value;
+  const g1 = greenInput.value;
+  const b1 = blueInput.value;
 
   if (outputHex.length > 3) {
     return (
       (document.body.style.backgroundColor = outputHex), hexToRgb(outputHex)
     );
-  } else if ((r.length > 0, g.length > 0, b.length > 0)) {
+  } else if (r.length > 0 && g.length > 0 && b.length > 0) {
     return (
       (document.body.style.backgroundColor = `rgb(${r}, ${g}, ${b})`),
       rgbToHex(r1, g1, b1)
@@ -62,14 +62,14 @@ function hexToRgb(clr) {
     rgb.push(parseInt(rgbHex[2], 16));
   }
 
-  inputR.value = rgb[0];
-  inputG.value = rgb[1];
-  inputB.value = rgb[2];
+  redInput.value = rgb[0];
+  greenInput.value = rgb[1];
+  blueInput.value = rgb[2];
 }
 
 // RGB TO HEX
 const rgbToHex = (r, g, b) => {
-  let convertedHex;
+  let convertedHex = undefined;
 
   r = parseInt(r).toString(16);
   g = parseInt(g).toString(16);
@@ -83,7 +83,7 @@ const rgbToHex = (r, g, b) => {
 };
 
 //ONLOAD RANDOM COLOR
-onload = (e) => {
+window.onload = (e) => {
   e.preventDefault();
   const randomColor = Math.floor(Math.random() * 16777216)
     .toString(16)
